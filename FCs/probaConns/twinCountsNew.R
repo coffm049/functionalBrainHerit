bd6fadf1edf85783c08a4dbf53af4015e8f91f08
Christian <coffm049@umn.edu>
2026-09-23 12:18:36 -0500

    for hpc sharing

--- FCs/probaConns/twinCountsNew.R
+++ FCs/probaConns/twinCountsNew.R
@@ -1,21 +1,24 @@
 library(tidyverse)
 library(mets)
 
-iteration=0 
-CHUNK = 20
-phenoNames <- paste0("o", (iteration * CHUNK) : ((iteration + 1) * CHUNK - 1))
+phenoNames <- c("o1")
+
 
 pheno <- read_parquet(
-  "/projects/standard/rando149/coffm049/ABCD/Workflow/02_Phenotypes/FCsTopo/probaConns.parquet",
+  # "/projects/standard/rando149/coffm049/ABCD/Workflow/02_Phenotypes/FCsTopo/probaConns.parquet",
+  "/scratch.global/coffm049/fctwinCheck/probaConns.parquet",,
   col_select = c("IID", phenoNames)
 ) %>% distinct(IID, .keep_all = TRUE)
 
 # Family IDs for twin pairing (IDs.txt has no header)
-IDs <- read_table("/projects/standard/rando149/coffm049/ABCD/Results/IDs/IDs.txt",
+IDs <- read_table(#"/projects/standard/rando149/coffm049/ABCD/Results/IDs/IDs.txt",
+      "/scratch.global/coffm049/fctwinCheck/IDs.txt",
                   col_names = c("FID", "IID"))
 pheno <- left_join(pheno, IDs, by = "IID") %>% distinct()
 
-df <- read_csv("/projects/standard/rando149/coffm049/ABCD/Workflow/02_Phenotypes/Covars2.csv") %>%
+df <- read_csv(#"/projects/standard/rando149/coffm049/ABCD/Workflow/02_Phenotypes/Covars2.csv"
+        "/scratch.global/coffm049/fctwinCheck/Covars2.csv"
+               ) %>%
   select(FID, IID, age, female, site_id_l, household.income, high.educ, genetic_zygosity_status_1) %>%
   mutate(zyg = case_when(
     grepl("mono", genetic_zygosity_status_1, ignore.case = TRUE) ~ "MZ",
@@ -25,17 +28,7 @@ df <- read_csv("/projects/standard/rando149/coffm049/ABCD/Workflow/02_Phenotypes
   select(-genetic_zygosity_status_1) %>%
   drop_na() %>%
   left_join(pheno, by = c("FID", "IID")) %>%
-  drop_na() %>%
-  pivot_longer(cols = starts_with("o"), names_to = "phenotype") %>%
-  nest(data = -phenotype) %>%
-  mutate(herit = map(data, function(d) {
-    tryCatch(
-      summary(twinlm(value ~ site_id_l + age + female + household.income + high.educ,
-                     data = as.data.frame(d), DZ = "DZ", zyg = "zyg", id = "FID", type = "ace")),
-      error = function(e) structure(list(error = conditionMessage(e)), class = "twinlm_error"))
-  }))
+  drop_na()
 
-out <- paste0("/users/4/coffm049/papers/functionalBrainHerit/results/FCs/probaConns/herit_", iteration, ".Rds")
-dir.create(dirname(out), recursive = TRUE, showWarnings = FALSE)
-saveRDS(df, out)
+mod <- summary(twinlm(value ~ site_id_l + age + female + household.income + high.educ, data = as.data.frame(df), DZ = "DZ", zyg = "zyg", id = "FID", type = "ace"))
 
